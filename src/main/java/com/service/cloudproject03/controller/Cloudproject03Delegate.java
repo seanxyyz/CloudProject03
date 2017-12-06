02f1537861c12d648d74011a645c6ca6579551a0
paas <paas@cse-codegen-backend-963378786-b13t3>
2017-12-06 17:43:54 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cloudproject03/controller/Cloudproject03Delegate.java
@@ -0,0 +1,14 @@
+package com.service.cloudproject03.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Cloudproject03Delegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
